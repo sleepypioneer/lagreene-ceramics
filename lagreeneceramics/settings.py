b3c8d6ddaa1c75e7843392cac3a2243a0be9c605
Jessica <jessica0greene@gmail.com>
2020-02-09 19:29:21 +0100

    Adds photologue

--- lagreeneceramics/settings.py
+++ lagreeneceramics/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'photologue',
+    'sortedm2m',
     'homepage.apps.HomepageConfig',
 ]
 
@@ -59,7 +63,7 @@ ROOT_URLCONF = 'lagreeneceramics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'lagreeneceramics/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +71,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'django.template.context_processors.i18n',
+                # 'django.template.context_processors.media',
+                # 'django.template.context_processors.static',
             ],
         },
     },
