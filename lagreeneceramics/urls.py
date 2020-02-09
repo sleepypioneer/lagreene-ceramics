1a41c7a8e90fa75531e0a846e297d0f5ae34a2a0
Jessica <jessica0greene@gmail.com>
2020-02-09 22:55:19 +0100

    Add static files to urls

--- lagreeneceramics/urls.py
+++ lagreeneceramics/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from photologue.sitemaps import GallerySitemap, PhotoSitemap
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include('homepage.urls')),
+    path('photologue/', include('photologue.urls', namespace='photologue')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
