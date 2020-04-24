d881ef64f4b97d9dc46bc5e065e826913741df92
sleepypioneer <jessica0greene@gmail.com>
2020-04-24 22:42:48 +0200

    add announcement for newsfeed

--- app/homepage/models.py
+++ app/homepage/models.py
@@ -7,3 +7,11 @@ class SlideShowItem(models.Model):
 
     class Meta(object):
         ordering = ['itemOrder']
+
+class Announcement(models.Model):
+    subject = models.CharField(max_length=75,  primary_key=True)
+    body = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="img/announcement")
+    link = models.URLField(max_length=100)
+    publish_date = models.DateTimeField()
+    end_date = models.DateTimeField()
