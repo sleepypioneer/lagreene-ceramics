0f836d86bc2fc925df9c7d724e13864c01feff72
sleepypioneer <jessica0greene@gmail.com>
2020-04-25 09:53:00 +0200

    Make description field in gallery not required

--- app/gallery/models.py
+++ app/gallery/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class GalleryItem(models.Model):
     image = models.ImageField(upload_to="img/gallery")
     title = models.CharField(max_length=75, primary_key=True)
-    description = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, blank=False, null=False)
     categories = models.ManyToManyField(Category, related_name='photos')
     archive = models.BooleanField()
     my_order = models.PositiveIntegerField(default=0, blank=False, null=False)
