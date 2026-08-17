4dd08e5a72088a09155a9fc77fa230448cdc03b8
jessica.greene <jessica.greene@ecosia.org>
2026-08-17 09:30:48 +0100

    date handling, empty state, add tests

--- app/homepage/views.py
+++ app/homepage/views.py
@@ -1,5 +1,7 @@
+from django.db.models import F
 from django.shortcuts import render
 from django.utils import timezone
+
 from homepage.models import SlideShowItem, Announcement
 from pages.models import Stockist
 
@@ -9,8 +11,8 @@ def homepage(request):
     slideshow = SlideShowItem.objects.all()
     announcements = Announcement.objects.all()
     upcoming_stockist = Stockist.objects.filter(
-        end_date__gte=timezone.now()
-    ).order_by('start_date').first()
+        end_date__gte=timezone.localdate()
+    ).order_by(F('start_date').asc(nulls_last=True)).first()
     context = {
         'slideshow': slideshow,
         'announcements': announcements,
